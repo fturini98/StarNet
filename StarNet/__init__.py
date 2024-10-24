a8a5526aaf0f1144fbea99c0e7bf89ff9f9c1bb8
Francesco Turini <fturini.turini7@gmail.com>
2024-10-24 12:46:01 +0200

    Correction of importing problems/Ereditary class

--- StarNet/__init__.py
+++ StarNet/__init__.py
@@ -1,1 +1,5 @@
-from .train import StarNet_CNN as CNN
+from .train import StarNet_CNN
+
+class CNN(StarNet_CNN):
+    def __init__(self):
+        super().__init__()  # Chiama il costruttore della classe padre
